944ddc657e195c23170384bba6dd039e67cb8bdc
faba-rem <github@remmel.onl>
2017-11-15 13:21:38 +0100

    initial

---
+++ AddMail.ps1
@@ -0,0 +1,40 @@
+﻿<#
+.SYNOPSIS
+    Add mail address to mailbox
+
+.DESCRIPTION
+    Skript to add new mail address to an exchange online mailbox
+
+.PARAMETER MailBox
+    defines mailbox where mail address should be added
+
+.PARAMETER Mail
+    defines new mailadress
+
+.NOTES
+    Version:        1.0
+    Author:         Fabian Remmel
+    Creation Date:  15.11.2017
+
+.EXAMPLE
+    .\AddMail.ps1 -MailBox Test -Mail test@test.de
+
+#>
+
+param(
+    [string]$MailBox,
+    [string]$Mail
+)
+
+#Get Admin Credentials
+$UserCredential = Get-Credential
+#Exchange Online Remot Powershell Session
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
+Import-PSSession $Session
+
+#Get Mailadresses from mailbox
+$tmp = (get-mailbox $MailBox).emailaddresses
+#Adds mailadress
+$tmp.add($Mail)
+#Add to Mailbox
+set-mailbox $MailBox -Emailaddresses $tmp
